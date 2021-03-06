38e9ad93e2fad15e2f2d95c5de92623f6431bd79
BorisTitarenko <borys.tytarenko.pz.2017@lpnu.ua>
2021-03-06 11:36:13 +0200

    Added Google authorization

---
+++ src/Login/Login.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { GoogleLogin } from 'react-google-login';
+
+const clientId = '803410015728-a0oqvinfell4a4is9parp8qeu7ka2bur.apps.googleusercontent.com';
+
+function Login() {
+  const onSuccess = (res) => {
+    console.log('Login Success: currentUser:', res.profileObj);
+    alert(
+      `Logged in successfully welcome ${res.profileObj.name} 😍. \n See console for full profile object.`
+    );
+  };
+
+  const onFailure = (res) => {
+    console.log('Login failed: res:', res);
+    alert(
+      `Failed to login. 😢`
+    );
+  };
+
+  return (
+    <div>
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={'single_host_origin'}
+        style={{ marginTop: '100px' }}
+        isSignedIn={true}
+      />
+    </div>
+  );
+}
+
+export default Login;
